f8cb13a4b5b5903b429a054efeb37a2d8097c22c
luminousott <143491172+luminousott@users.noreply.github.com>
2025-07-06 13:51:03 +0800

    Update Extension.php

--- src/Extension.php
+++ src/Extension.php
@@ -2,23 +2,19 @@
 
 namespace Just404\DisableEmailNotifications;
 
-use Flarum\Extend;
+use Flarum\Extend\ExtenderInterface;
+use Flarum\Extension\Extension as FlarumExtension;
+use Flarum\Notification\Event\Sending;
+use Just404\DisableEmailNotifications\Listeners\DisableEmailNotifications;
+use Illuminate\Contracts\Container\Container;
 
-return [
-    (new Extend\Frontend('forum'))
-        ->js(__DIR__ . '/../js/dist/forum.js') // 如果扩展有前端JS
-        ->css(__DIR__ . '/../less/forum.less'), // 如果扩展有前端CSS
-
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__ . '/../js/dist/admin.js') // 如果扩展有后台JS
-        ->css(__DIR__ . '/../less/admin.less'), // 如果扩展有后台CSS
-
-    new Extend\Locales(__DIR__ . '/../locale'), // 如果有语言文件
-
-    // 注册事件监听器
-    (new Extend\Event())
-        ->listen(
-            \Flarum\Notification\Event\Sending::class,
-            Listeners\DisableEmailNotifications::class
-        ),
-];
+class Extension implements ExtenderInterface
+{
+    public function extend(Container $container, FlarumExtension $extension = null)
+    {
+        $container->make('events')->listen(
+            Sending::class,
+            DisableEmailNotifications::class
+        );
+    }
+}
