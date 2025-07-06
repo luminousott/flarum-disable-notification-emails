aca0af08200e98ad9e0e31a3746c875d75a5e5c5
luminousott <143491172+luminousott@users.noreply.github.com>
2025-07-06 13:53:41 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,12 +1,1 @@
-<?php
 
-namespace Just404\DisableEmailNotifications;
-
-use Flarum\Extend;
-use Flarum\Notification\Event\Sending;
-use Just404\DisableEmailNotifications\Listeners\DisableEmailNotifications;
-
-return [
-    (new Extend\Event())
-        ->listen(Sending::class, DisableEmailNotifications::class)
-];
